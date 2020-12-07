8e2f1c9c732dcae43217aea9a409e8e4006ef759
S00197749 Luke Sweeney <S00197749@mail.itsligo.ie>
2020-12-07 12:03:37 +0000

    Created Employee class, 2 FullTimeEmployee objects and 2 PartTimeEmployee objects

---
+++ Employee App/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_App
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Employee(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{LastName}, {FirstName}");
+        }
+        public abstract decimal CalculateMonthlyPay();
+        
+    }
+    public class FullTimeEmployee : Employee
+    {
+        public decimal Salary { get; set; }
+        public FullTimeEmployee(string firstname, string lastname, decimal salary) : base(firstname, lastname)
+        {
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{LastName.ToUpper()}, {FirstName} - Full Time");
+        }
+
+        public override decimal CalculateMonthlyPay()
+        {
+            return Salary / 12;
+        }
+    }
+    public class PartTimeEmployee : Employee
+    {
+        public decimal HourlyRate { get; set; }
+        public double HoursWorked { get; set; }
+        public PartTimeEmployee(string firstname, string lastname, decimal hourlyrate, double hoursworked) : base(firstname, lastname) 
+        {
+            HourlyRate = hourlyrate;
+            HoursWorked = hoursworked;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{LastName.ToUpper()}, {FirstName} - Part Time");
+        }
+
+        public override decimal CalculateMonthlyPay()
+        {
+            return HourlyRate * (decimal)HoursWorked;
+        }
+    }
+}
--- Employee App/MainWindow.xaml.cs
+++ Employee App/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,16 @@ namespace Employee_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Employee> employee = new ObservableCollection<Employee>();
         public MainWindow()
         {
             InitializeComponent();
+
+            FullTimeEmployee em1 = new FullTimeEmployee("Jess", "Walsh", 50000);
+            FullTimeEmployee em2 = new FullTimeEmployee("Joe", "Murphy", 100000);
+
+            PartTimeEmployee em3 = new PartTimeEmployee("John", "Smith", 15, 10);
+            PartTimeEmployee em4 = new PartTimeEmployee("Jane", "Jones", 13, 15);
         }
     }
 }
